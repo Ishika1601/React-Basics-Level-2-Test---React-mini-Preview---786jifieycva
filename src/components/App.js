2abaa29505f7454adfb387d0b48bd1dec62bdb7c
Ishika1601 <98034508+Ishika1601@users.noreply.github.com>
2022-04-15 20:54:14 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,37 @@
-import React from 'react'
+import React, { useState } from "react";
 import '../styles/App.css';
 import { Preview } from './Preview';
 const App = () => {
 
+  const [contentInput, setContentInput] = useState("hello newton");
+  const [fontSizeInput, setFontSizeInput] = useState(10);
+  const [paddingInput, setPaddingInput] = useState(5);
+  
   return (
     <div id="main">
       <div>
-        {/* add input elememnts inside here */}
+        <input
+          type="text"
+          id="contentInput"
+          value={contentInput}
+          onChange={(e) => setContentInput(e.target.value)}
+        />
+        <br />
+        <input
+          type="number"
+          id="fontSizeInput"
+          value={fontSizeInput}
+          onChange={(e) => setFontSizeInput(e.target.value)}
+        />
+        <br />
+        <input
+          type="number"
+          id="paddingInput"
+          value={paddingInput}
+          onChange={(e) => setPaddingInput(e.target.value)}
+        />
       </div>
- 
- {/* render Preview component here */}
- 
+      <Preview content={contentInput} fontSize={`${fontSizeInput}px`} padding={`${paddingInput}px`}/>
     </div>
   )
 }
